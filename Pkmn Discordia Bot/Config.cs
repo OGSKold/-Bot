69d7bce34b9ed1022e2e88f39ea59084c64542e3
Talha Qamar <boooooooofys@gmail.com>
2018-06-07 10:37:07 +0500

    Initial commit

---
+++ Pkmn Discordia Bot/Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Pkmn_Discordia_Bot
+{
+    class Config
+    {
+        private const string configfolder = "Resources";
+        private const string configfile = "config.json";
+
+        public static BotConfig bot;
+        static Config()
+        {
+            if (!Directory.Exists(configfolder)) Directory.CreateDirectory(configfolder);
+
+            if (!File.Exists(configfolder + "/" + configfile))
+            {
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot,Formatting.Indented);
+                File.WriteAllText(configfolder + "/" + configfile, json);
+            }
+            else
+            {
+                string json = File.ReadAllText(configfolder + "/" + configfile);
+                var data = JsonConvert.DeserializeObject<BotConfig>(json);
+            }
+        }
+    }
+   public struct BotConfig
+    {
+        public string token;
+        public string cmdPrefix;
+    }
+}
